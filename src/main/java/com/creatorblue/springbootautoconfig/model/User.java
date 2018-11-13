c02636d07ce9f916b02ec84201f9af9e536d7025
beanbean <email@example.com>
2018-11-13 15:33:57 +0800

    提交了用户信息

---
+++ src/main/java/com/creatorblue/springbootautoconfig/model/User.java
@@ -0,0 +1,16 @@
+package com.creatorblue.springbootautoconfig.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+public class User implements Serializable{
+    private Long id;
+    private String username;
+    private String password;
+}
