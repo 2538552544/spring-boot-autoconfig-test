90bc4d9a12037d26a533106514cac1f5233b1b5c
beanbean <email@example.com>
2018-11-13 17:08:54 +0800

    提交修改文件内容

--- src/main/java/com/creatorblue/springbootautoconfig/controller/UpLoadFileController.java
+++ src/main/java/com/creatorblue/springbootautoconfig/controller/UpLoadFileController.java
@@ -6,26 +6,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class UpLoadFileController {
     @PostMapping("uploadFile")
     @ResponseBody
     public String uploadFile(@RequestParam("file") MultipartFile file) {
-        if(file.isEmpty()){
+        if (file.isEmpty()) {
             return "文件不能为空";
         }
         String fileName = file.getOriginalFilename();
         //获取文件的大小
         int size = (int) file.getSize();
         System.out.println(size);
-        String path = "F:/IOTest";
-        File desc = new File(path+"/"+fileName);
+        String path = "D:/IOTest";
+        File desc = new File(path + "/" + fileName);
         //如果夫目录不存在 则创建
-        if(desc.getParentFile().exists()){
+        if (desc.getParentFile().exists()) {
             desc.getParentFile().mkdir();
         }
         try {
@@ -40,5 +43,29 @@ public class UpLoadFileController {
     @GetMapping("uploadFile")
     public String uploadFileHtml() {
         return "upload";
+    }
+
+    @GetMapping("uploadFiles")
+    public String uploadFilesHtml() {
+        return "uploads";
+    }
+
+    @PostMapping("uploadFiles")
+    @ResponseBody
+    public String uploadFiles(HttpServletRequest request) throws IOException {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        if (files.isEmpty()) {
+            return "false";
+        }
+        String path = "D:/IOTest";
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) {
+                return "false";
+            }
+            System.out.println(file.getOriginalFilename());
+            File desc = new File(path + "/" + file.getOriginalFilename());
+            file.transferTo(desc);
+        }
+        return "true";
     }
 }
