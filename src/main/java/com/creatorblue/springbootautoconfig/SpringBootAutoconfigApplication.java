2fc20f34c5a350788007592455c135122be152da
beanbean <email@example.com>
2018-11-13 15:12:22 +0800

    first commit

---
+++ src/main/java/com/creatorblue/springbootautoconfig/SpringBootAutoconfigApplication.java
@@ -0,0 +1,12 @@
+package com.creatorblue.springbootautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutoconfigApplication.class, args);
+	}
+}
